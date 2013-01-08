fab34bfa389d80578b79bdfed505bcd20c21ad1d
Zachary Cutlip <zcutlip@tacnetsol.com>
2013-01-07 21:19:26 -0500

    Removed placeholder overflow string pattern.

--- overflow_development/overflowbuilder.py
+++ overflow_development/overflowbuilder.py
@@ -29,20 +29,8 @@ class OverflowBuffer:
     overflow_sections -- List of OverflowSection and RopGadget objects to substitute
         into the base overflow string.
     """
-    #TODO: Means of specify a list of bad chars to avoid and to check for.
-    """
-    overflow_string=\
-        "Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0A"+\
-        "c1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae"+\
-        "2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag2Ag3"+\
-        "Ag4Ag5Ag6Ag7Ag8Ag9Ah0Ah1Ah2Ah3Ah4Ah5Ah6Ah7Ah8Ah9Ai0Ai1Ai2Ai3Ai4A"+\
-        "i5Ai6Ai7Ai8Ai9Aj0Aj1Aj2Aj3Aj4Aj5Aj6Aj7Aj8Aj9Ak0Ak1Ak2Ak3Ak4Ak5Ak"+\
-        "6Ak7Ak8Ak9Al0Al1Al2Al3Al4Al5Al6Al7Al8Al9Am0Am1Am2Am3Am4Am5Am6Am7"+\
-        "Am8Am9An0An1An2An3An4An5An6An7An8An9Ao0Ao1Ao2Ao3Ao4Ao5Ao6Ao7Ao8A"+\
-        "o9Ap0Ap1Ap2Ap3Ap4Ap5Ap6Ap7Ap8Ap9Aq0Aq1Aq2Aq3Aq4Aq5Aq6Aq7Aq8Aq9Ar"+\
-        "0Ar1Ar2Ar3Ar4Ar5Ar6Ar7Ar8Ar9As0As1As2As3As4As5As6As7As8As9At0At1"+\
-        "At2At3At4At5At6At7At8At9"
-    """  
+    #TODO: Means of specifying a list of bad chars to avoid and to check for.
+    
     @classmethod        
     def pattern_create(cls,requested_length):
 
