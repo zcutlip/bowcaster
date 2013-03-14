7d9aaa3f24961e6f1f86e7b472da25017a8ffeb0
Zachary Cutlip <zcutlip@tacnetsol.com>
2013-03-14 12:10:16 -0400

    Documentation and captitalization of constants.

--- src/crossbow/payloads/mips/trampoline.py
+++ src/crossbow/payloads/mips/trampoline.py
@@ -4,8 +4,17 @@ from ...common.support import BigEndian,LittleEndian
 
 
 class Trampoline(object):
-    MaxBackJump=-0x20000
-    MaxFwdJump=0x1fffc
+    """
+    A small (8 byte) trampoline payload.
+    
+    Attributes
+    ----------
+    MAX_BACK_JUMP: The maximum negative offset that can be encoded into the branch.
+    MAX_FWD_JUMP: The maximum positive offset that can be encoded into the branch.
+    
+    """
+    MAX_BACK_JUMP=-0x20000
+    MAX_FWD_JUMP=0x1fffc
     shellcodes={}
     shellcodes[BigEndian] = string.join([
         "\x11\xefHIHILOLO",    # beq	t7,t7,0xffff0010
@@ -15,14 +24,28 @@ class Trampoline(object):
         "LOLOHIHI\xef\x11",    # beq    t7,t7,0x20000
         "\x26\x78\xe0\x01"    # xor t7,t7,zero
     ],'')
+    
     def __init__(self,endianness,offset):
-        if (offset < self.__class__.MaxBackJump or 
-                offset > 0x1fffc):
-            raise Exception("Offst %d is outside of %d backwards or %d forwards." % 
-                    (offset,self.__class__.MaxBackJump,self.__class__.MaxFwdJump))
-        self.endianness=endianness       
+        """
+        Class constructor.
+        
+        Parameters
+        ----------
+        endianness: Endianness of the target system. (See the support.BigEndian
+            and support.LittleEndian)
+        offset: Positive or negative offset to jump.
+        
+        Raises EncoderException if the offset is outside of the MAX_BACK_JUMP - 
+            MAX_FWD_JUMP range.
+        """
+        if (offset < self.__class__.MAX_BACK_JUMP or 
+                offset > self.__class__.MAX_FWD_JUMP):
+            raise EncoderException("Offset %d is outside of %d backwards or %d forwards." % 
+                    (offset,self.__class__.MAX_BACK_JUMP,self.__class__.MAX_BACK_JUMP))
+        self.endianness=endianness
         self.shellcode=self.__class__.shellcodes[endianness]
-
+        
+        #TODO: is this endianness-safe?
         packedbytes=struct.pack(">h",(offset>>2))
         
         low_byte=packedbytes[1]
