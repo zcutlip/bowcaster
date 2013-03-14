8f2bd529fe1e6f29f347fd2735a029027fa7d061
Zachary Cutlip <zcutlip@tacnetsol.com>
2013-03-14 12:06:59 -0400

    Changed refs to simplesploit to crossbow in setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 
 from distutils.core import setup
 
-setup(name='Simplesploit',
+setup(name='Crossbow',
         version='0.1',
         description='Simple exploit development framework',
         author='Zachary Cutlip',
         package_dir = {'':'src'},
-        packages=['simplesploit',
-            'simplesploit.common',
-            'simplesploit.overflow_development',
-            'simplesploit.payloads',
-            'simplesploit.payloads.mips',
-            'simplesploit.encoders',
-            'simplesploit.servers']
+        packages=['crossbow',
+            'crossbow.common',
+            'crossbow.overflow_development',
+            'crossbow.payloads',
+            'crossbow.payloads.mips',
+            'crossbow.encoders',
+            'crossbow.servers']
         )
 
