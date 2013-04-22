a517ec00f6f216f0d0ec910d32dc8d03428c7e9b
Zachary Cutlip <uid000@gmail.com>
2013-04-22 15:51:19 -0400

    changed some tabs to spaces in http.py

--- src/bowcaster/clients/http.py
+++ src/bowcaster/clients/http.py
@@ -16,7 +16,7 @@ class HttpClient(object):
     def encode(self,data):
         """
         If you need to control what data is URL encoded, use this, otherwise set
-		urlencode=True on send()
+        urlencode=True on send()
         """
         if type(data) == dict:
             data=urllib.urlencode(data)
@@ -25,20 +25,20 @@ class HttpClient(object):
         return data
         
     def send(self,url,headers=None,post_data=None,urlencode=False,get_resp=True):
-		"""
-		Send HTTP data.  If post data is provided, the request is sent as a POST,
-		otherwise it is sent as a GET.
-		
+        """
+        Send HTTP data.  If post data is provided, the request is sent as a POST,
+        otherwise it is sent as a GET.
+        
         Parameters
         ----------
-		url:		URL to send the GET or POST to.
-		headers: 	Optional. A dictionary of header key/value pairs describing
-					the HTTP headers to send. Ordering is not guaranteed.
-		post_data:	Optional. Data to send in a POST request. Defaults to None.
-		urlencode:	Optional. URL encode the POST data. Defaults to False.
-		get_resp:	Optional. A response is expected, and will be read() and
-					returned.  Defaults to true.
-		"""
+        url:        URL to send the GET or POST to.
+        headers:    Optional. A dictionary of header key/value pairs describing
+                    the HTTP headers to send. Ordering is not guaranteed.
+        post_data:  Optional. Data to send in a POST request. Defaults to None.
+        urlencode:  Optional. URL encode the POST data. Defaults to False.
+        get_resp:   Optional. A response is expected, and will be read() and
+                    returned.  Defaults to true.
+        """
         if post_data and urlencode:
             post_data=self.encode(post_data)
             
