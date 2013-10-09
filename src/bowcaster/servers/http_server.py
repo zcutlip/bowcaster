a2fc78a7d68c3f3e7d8aff3a3eeb94e8ed62e44a
Zachary Cutlip <uid000@gmail.com>
2013-10-09 10:19:44 -0400

    almost complete rewrite of http_server module.
    * now support asynchronous use like the other server modules by forking into the background.
    * instantiate httpd object
    * call httpd.serve()
    * do other stuff
    * call httpd.wait() or
    * call httpd.shutdown() to shutdown prematurely

--- src/bowcaster/servers/http_server.py
+++ src/bowcaster/servers/http_server.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright (c) 2013
 # - Zachary Cutlip <uid000@gmail.com>
 # - Tactical Network Solutions, LLC
@@ -6,13 +5,135 @@
 # See LICENSE.txt for more details.
 # 
 
-import os
 
+import sys
+import signal
+import os
 import urlparse
+import time
+import traceback
+import errno
 from BaseHTTPServer import HTTPServer,BaseHTTPRequestHandler
 
-class LimitedHTTPServer(HTTPServer):
-    def __init__(self,server_address,handler_class,docroot=None,files_to_serve=[]):
+from ..servers import ServerException
+from ..common.support import Logging
+
+
+class HTTPConnectbackServer(object):
+    """
+    """
+    def __init__(self,connectback_ip,files_to_serve,port=8080,logger=None):
+        if not logger:
+            logger=Logging()
+        self.logger=logger
+        self.server_address=(connectback_ip,port)
+        self.files_to_serve=files_to_serve
+        problems=self._sanity_check_files(files_to_serve)
+        if len(problems) > 0:
+            msg="There were problems with the following files:"
+            for file,e in problems.items():
+                msg +="\n\t%s: %s" % (file,str(e))
+            raise ServerException(msg)
+        
+        
+    def _sanity_check_files(self,files):
+        problems={}
+        for file in files:
+            try:
+                open(file,"r")
+            except Exception as e:
+                problems[file]=e
+
+        return problems
+
+    
+    def _exit(self):
+        #this prevents a hang in mod_wsgi, which traps sys.exit()
+        try:
+            os.execv("/bin/true",["/bin/true"])
+        except:
+            sys.exit()
+    
+    def _handler(self,signum,frame):
+        if self.keepgoing:
+            self.keepgoing=False
+            raise ServerException()
+
+    def _setup_signals(self):
+        self.keepgoing=True
+        signal.signal(signal.SIGINT,self._handler)
+        signal.signal(signal.SIGTERM,self._handler)
+
+    def _serve_files(self):
+        while self.keepgoing and self.httpd.more_files():
+            try:
+                self.httpd.handle_request()
+            except:
+                self.logger.LOG_INFO("[%d] http server caught an exception." % os.getpid())
+                #traceback.print_exc()
+                self.keepgoing=False
+
+    
+    def wait(self):
+        if not self.pid:
+            return None
+        self._setup_signals()
+        keepgoing=True
+        status=(0,0)
+        while keepgoing:
+            try:
+                status=os.waitpid(self.pid,0)
+            except Excpetion as e:
+                keepgoing=False
+        self.pid=None
+        
+        return status[1]
+    
+    def shutdown(self):
+        if not self.pid:
+            return
+        self.logger.LOG_INFO("[%d] Shutting down server. PID: %d" % (os.getpid(),self.pid))
+        #traceback.print_stack()
+        try:
+            os.kill(self.pid,signal.SIGTERM)
+        except OSError as ose:
+            if ose.errno == errno.ESRCH:
+                return
+            else:
+                self.logger.LOG_WARN("Error shutting down server: %s" % str(ose))
+                raise
+        
+    def serve(self):
+        try:
+            self.httpd=_LimitedHTTPServer(self.server_address,
+                                         _LimitedHTTPRequestHandler,
+                                         files_to_serve=self.files_to_serve)
+        except Exception as e:
+            self.logger.LOG_WARN("There was an error creating the HTTP server: %s" % str(e))
+            raise
+        self.pid=os.fork()
+        if self.pid:
+            self.httpd.socket.close()
+            self.httpd=None
+            return self.pid
+        else:
+            self._setup_signals()
+            self._serve_files()
+        
+        self.logger.LOG_INFO("[%d] Shutting down." % os.getpid())
+        
+        self.httpd.socket.close()
+        self.httpd=None
+        self._exit()
+        
+
+
+class _LimitedHTTPServer(HTTPServer):
+    def __init__(self,server_address,handler_class,docroot=None,files_to_serve=[],logger=None):
+        if not logger:
+            logger=Logging()
+        self.logger=logger
+        
         self.docroot=docroot
         if not docroot:
             self.docroot=os.getcwd()
@@ -20,10 +141,20 @@ class LimitedHTTPServer(HTTPServer):
         for filename in files_to_serve:
             if not filename.startswith("/"):
                 filename="/"+filename
+                filename=self._sanitize_filename(filename)
+            filename=self.docroot+filename
             self.files_to_serve.append(filename)
 
         HTTPServer.__init__(self,server_address,handler_class)
+    
+    def _sanitize_filename(self,filename):
+        while '/../' in filename:
+            filename=filename.replace('/../','/')
+        
+        return filename
+    
     def has_file(self,filename):
+        logger=self.logger
         if filename in self.files_to_serve:
             return True
         return False
@@ -37,56 +168,41 @@ class LimitedHTTPServer(HTTPServer):
         if self.has_file(filename):
             self.files_to_serve.remove(filename)
 
-class LimitedHTTPRequestHandler(BaseHTTPRequestHandler):
-    def __sanitize_path__(self):
-        while '/../' in self.path:
-            self.path=self.path.replace("/../","/")
+class _LimitedHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def log_message(self, fmt, *args):
+        self.server.logger.LOG_DEBUG(fmt % (args))
 
     def do_GET(self):
-        print self.server.docroot
-        self.__sanitize_path__()
-        print self.path
-        print urlparse.urlparse(self.path)
-        file_exists=True
-        if self.server.has_file(self.path):
+        if self.server.logger:
+            logger=self.server.logger
+        else:
+            logger=Logging()
+        filename=self.server.docroot+self.path
+
+
+        file_exists=False
+        if self.server.has_file(filename):
+            file_exists=True
+            self.server.remove_file(filename)
             try:
-                f=open(self.server.docroot+self.path)
+                f=open(filename)
                 self.send_response(200)
                 self.send_header('Content-type','application/octet-stream')
                 self.end_headers()
                 self.wfile.write(f.read())
                 f.close()
-                self.server.remove_file(self.path)
             except Exception as e:
-                print e
+                logger.LOG_WARN("Error serving file: %s" % self.path)
+                logger.LOG_WARN("%s" % str(e))
                 file_exists=False
         else:
-            print "Server doesn't have file: %s" % self.path
-            file_exists=False
+            logger.LOG_WARN("Server doesn't have file: %s" % self.path)
+
 
         if not file_exists:
             self.send_error(404,'File Not found: %s' % self.path)
 
 
 
-if __name__ == "__main__":
-    server_address=('',8080)
-    httpd=LimitedHTTPServer(server_address,LimitedHTTPRequestHandler,files_to_serve=["file1","file2"])
-    #httpd=LimitedHTTPServer(server_address,LimitedHTTPRequestHandler,files_to_serve=[])
-    keep_going=True
-    try:
-        while keep_going:
-            if httpd.more_files():
-                print "Handling request"
-                httpd.handle_request()
-            else:
-                print "No more files."
-                keep_going=False
-    except KeyboardInterrupt:
-        print "Interrupted."
-        keep_going=False
-
-
-    print "Shutting down"
-    httpd.socket.close()
 
