dec75a0ad3eaa2df76a9359c96a033c7fc041916
Zachary Cutlip <uid000@gmail.com>
2013-11-25 14:18:56 -0500

    added hackers module for random Hackers(1995) quotes.

---
+++ src/bowcaster/common/hackers/hackers.py
@@ -0,0 +1,47 @@
+import random
+import os
+from ...common.support import Logging
+
+class Hackers():
+    HACKERS_FILE="hackers.txt"
+    STARS="*************************"
+    
+    def __get_this_path(self):
+        thisfile=__file__
+        if os.path.islink(thisfile):
+            thisfile=os.path.realpath(thisfile)
+
+        thispath=os.path.dirname(os.path.abspath(thisfile))
+        return thispath
+    
+    def __init__(self,logger=None):
+        cls=self.__class__
+        thispath=self.__get_this_path()
+        if not logger:
+            logger=Logging()
+        self.logger=logger
+        self.hackers_quotes=[]
+        
+        hackers_file="%s/%s" % (thispath,cls.HACKERS_FILE)
+        
+        for line in open(hackers_file,"rb").readlines():
+            self.hackers_quotes.append(line.rstrip())
+    def banner(self):
+        self.logger.LOG_INFO("Hackers quotes enabled.")
+        
+    def random_quote(self):
+        random.seed()
+        rand=random.randint(1,len(self.hackers_quotes))-1
+        return self.hackers_quotes[rand]
+        
+        
+    def log_random_quote(self,logger=None):
+        if not logger:
+            logger=self.logger
+            
+        quote=self.random_quote()
+        logger.LOG_DEBUG("%s" % self.__class__.STARS)
+        logger.LOG_DEBUG("Hackers random movie quote:")
+        logger.LOG_DEBUG("%s" % quote)
+        logger.LOG_DEBUG("%s" % self.__class__.STARS)
+        
