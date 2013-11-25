a75d7204e19d09e66f7abd46189299704017e92e
Zachary Cutlip <uid000@gmail.com>
2013-11-25 14:19:43 -0500

    initialize hackers module in common.__init__.py if HACKERS_QUOTES_ENABLED is True

--- src/bowcaster/common/__init__.py
+++ src/bowcaster/common/__init__.py
@@ -6,8 +6,15 @@
 # 
 
 from support import *
+from hackers.hackers import *
 
 __all__ = ["BigEndian","LittleEndian","Logging"]
 
-
-
+hackers_quotes=None
+try:
+    hacker_quotes_enabled=getattr(sys.modules['__main__'],'HACKERS_QUOTES_ENABLED')
+except:
+    hacker_quotes_enabled=False
+if True == hacker_quotes_enabled:
+    hackers_quotes=Hackers()
+    hackers_quotes.banner()
