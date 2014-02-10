4c609ef9813c52877d8bf075448c980bbfa677cc
Zachary Cutlip <uid000@gmail.com>
2014-02-10 10:10:32 -0500

    added max_level optional parameter to Logging class

--- src/bowcaster/common/support.py
+++ src/bowcaster/common/support.py
@@ -7,6 +7,13 @@
 import sys
 import binascii
 
+def hex_string(string):
+    h_string=""
+    for byte in string:
+        h_string+="\\x"+binascii.hexlify(byte)
+
+    return h_string
+
 def pretty_string(string):
     p_string=""
     for byte in string:
@@ -49,10 +56,14 @@ class Logging:
     prefixes.append(" [+] ")
     prefixes.append(" [@] ")
 
-    def __init__(self,logfile=None):
+    def __init__(self,logfile=None,max_level=INFO):
+        self.max_level=int(max_level)
         self.logfile=sys.stdout
         if logfile:
             self.logfile=open(logfile,"a")
+
+    def set_max_log_level(self,max_level):
+        self.max_level=max_level
 
     def log_msg_start(self,msg,level=INFO):
         """
@@ -63,23 +74,26 @@ class Logging:
         msg: String to print
         level: one of the Logging class's level attributes
         """
-        pref=Logging.prefixes[level]
-        self.logfile.write(pref+msg)
-        self.logfile.flush()
+        if level <= self.max_level:
+            pref=Logging.prefixes[level]
+            self.logfile.write(pref+msg)
+            self.logfile.flush()
 
-    def log_msg_end(self,msg):
+    def log_msg_end(self,msg,level=INFO):
         """
         Print the start of a log message with NO level decorator, but with a newline.
         """
-        self.logfile.write("%s\n" % msg)
-        self.logfile.flush()
+        if level <= self.max_level:
+            self.logfile.write("%s\n" % msg)
+            self.logfile.flush()
 
     def log_msg(self,msg,level=INFO):
         """
         Print a log message prefixed with level decorator and terminated with a newline.
         """
-        msg="%s\n"%msg
-        self.log_msg_start(msg,level)
+        if level <= self.max_level:
+            msg="%s\n"%msg
+            self.log_msg_start(msg,level)
 
     def LOG_INFO(self,msg):
         """
